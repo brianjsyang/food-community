f39cf33860d5e0268231bd09b4884cc58eb99a20
Brian Yang <brianjsyang@gmail.com>
2024-02-20 22:36:31 -0800

    bug fix to allow data submission on production

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,3 +1,7 @@
+'use client'
+
+import { useFormState } from 'react-dom';
+
 import ImagePicker from '@/components/meals/image-picker';
 import { shareMeal } from '@/lib/actions';
 import MealsFormSubmit from '@/components/meals/meals-form-submit';
@@ -5,6 +9,9 @@ import classes from './page.module.css';
 
 
 export default function ShareMealPage() {
+  const [returnState, dispatch] = useFormState(shareMeal, { message: null }); // (formAction, initial value)
+
+
   return (
     <>
       <header className={classes.header}>
@@ -14,7 +21,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form} action={shareMeal}>
+        <form className={classes.form} action={dispatch}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -43,6 +50,8 @@ export default function ShareMealPage() {
             ></textarea>
           </p>
           <ImagePicker label="Your image" name="image" />
+          { returnState.message && <p>{returnState.message}</p> }
+
           <p className={classes.actions}>
             <MealsFormSubmit />
           </p>
